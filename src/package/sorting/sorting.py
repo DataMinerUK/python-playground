b77428f88d4705f41c2ecbaf31d248f9d357bfd8
Nicola Hughes <nicolarhughes@gmail.com>
2020-06-06 14:43:04 -0700

    Bubble sort

--- src/package/sorting/sorting.py
+++ src/package/sorting/sorting.py
@@ -6,3 +6,11 @@ def selection_sort(array):
                  min_index = j
         array[i], array[min_index] = array[min_index], array[i]
     return array
+
+def bubble_sort(array):
+    n = len(array)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if array[j] > array[j+1]:
+                array[j], array[j+1] = array[j+1], array[j]
+    return array
---
+++ tests/sorting/test_sorting.py
@@ -0,0 +1,16 @@
+import pytest
+import unittest
+
+from src.package.sorting.sorting import *
+
+class TestSorting(unittest.TestCase):
+
+    def test_selection_sort(self):
+        a = [12, 7, 4, 9, 34]
+        expected = [4, 7, 9, 12, 34]
+        self.assertEqual(selection_sort(a), expected)
+
+    def test_bubble_sort(self):
+        a = [12, 7, 4, 9, 34]
+        expected = [4, 7, 9, 12, 34]
+        self.assertEqual(bubble_sort(a), expected)
